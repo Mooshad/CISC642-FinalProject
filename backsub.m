9b0b0b2130a67f94efd644e138a19bd5ed1c62a6
schmittlema <schmttle@udel.edu>
2016-12-09 13:47:53 -0500

    results!

--- backsub.m
+++ backsub.m
@@ -1,21 +1,25 @@
-one = rgb2gray(imread('1.JPG'));
-two = rgb2gray(imread('2.JPG'));
-three = rgb2gray(imread('3.JPG'));
-four = rgb2gray(imread('4.JPG'));
-five = rgb2gray(imread('5.JPG'));
+function [ output_args ] = backsub( iarray,t )
+%performs background subtraction,erosion,bwlabel
+[sizeb,sizea] = size(iarray);
+curr = iarray{1};
+for s=2:sizea
+    diff = abs(curr-iarray{s});
+    result = thres(diff,t);
+end
+  
 
-[r,c] = size(one);
-result = zeros(r,c);
-
-result = imadd((imadd((imadd((imadd(one,two)/2),three)/2),four)/2),five)/2;
-result2 = imadd(imadd(imadd(imadd(one,two),three),four),five)/5;
-result3 = mean([mean2(one),mean2(two),mean2(three),mean2(four),mean2(five)]);
-
-
-imshow(result);
+imshow(iarray{end});
 figure
-imshow(3*imsubtract(four,result3));
-% figure
-% imshow(four-mean2(result2));
-% figure
-% imshow(four);
+
+se = offsetstrel('ball',5,5);
+erodedI = imerode(result,se);
+%h = fspecial('average',11);
+erodedI = medfilt2(imerode(erodedI,se));
+[c,num_heads] = bwlabel(erodedI);
+imshow(c);
+num_heads
+
+
+
+end
+
