2e3a38df7634f0b4c1552592745cad7998af9a0c
Mooshad <jakep@udel.edu>
2016-12-09 00:25:02 -0500

    the final countdown dudududu. dududududuuuuu

--- classsify.m
+++ classsify.m
@@ -1,5 +1,21 @@
-function [ result ] = classify( point,num )
-%sdaf
-result = 0;
+function [ c ] = classify( point,num )
+% classifies a given point and determines if it is a head
+% or not a head
+xyz = point{2};
+
+if num == 1
+    if xyz(1) > -1.7
+        c = 0;
+    end;
+elseif num == 2
+    if xyz(3) > .1
+        c = 0;
+    end;
+elseif num == 3
+    if xyz(1) > -.5 && xyz(1) < .0 && xyz(3) > .1 && xyz(3) < .5
+        c = 1;
+    end;
+end;
+
 end
 
